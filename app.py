53a6abebef9b6a29d6e932954a182349dbef738c
Sagar Prasad <sagarprasad63574@gmail.com>
2023-03-05 18:02:49 -0800

    ok

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.register_blueprint(api, url_prefix="")
 app.register_blueprint(favorites, url_prefix="")
 app.register_blueprint(mylist, url_prefix="")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///superhero-app'))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///superhero-app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
